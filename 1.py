3789f801b1b13b2ca79643c99fbd217a600d21ab
AMAN-SONI-55566 <amanoffcial55566@gmail.com>
2025-05-31 23:54:28 +0530

    Initial commit

---
+++ 1.py
@@ -0,0 +1,4 @@
+#this is a simple Python script that prints "Hello, World!" to the console.
+print("Hello, World!")
+name=input("enter your name:")
+print("nice to meet you", name )
---
+++ 2.py
@@ -0,0 +1,31 @@
+
+# def main():                                     Defines the function.
+#     print("Hello, World!")
+#     name=input("enter your name:")
+#     print("nice to meet you", name )
+    
+# if __name__ == "__main__":                      Checks if the script is being run directly.
+#     main()
+
+# program to show basic data types in python 
+# int, float, str, bool, list, tuple, set, dict
+myint = 10
+myfloat = 20.5
+mystr = "Hello, World!"
+mybool = True
+mylist = [1, 2, 3, 4, 5]
+mytuple = (1, 2, 3, 4, 5)
+myset = {1, 2, 3, 4, 5}
+mydict = {"name": "John", "age": 30, "city": "New York"}
+
+def main():
+    print("Integer:", myint)
+    print("Float:", myfloat)
+    print("String:", mystr)
+    print("Boolean:", mybool)
+    print("List:", mylist)
+    print("Tuple:", mytuple)
+    print("Set:", myset)
+    print("Dictionary:", mydict)
+    
+    
