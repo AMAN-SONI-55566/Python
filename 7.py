baec63a8b41bef5ab640a9daaf60954ff02d6355
AMAN-SONI-55566 <amanoffcial55566@gmail.com>
2025-06-20 09:37:01 +0530

    Use the print() function to output a string

---
+++ 7.py
@@ -0,0 +1,10 @@
+#using print fumction to output a string.
+print("I am learning PYTHON from Aman's Repository")
+
+#syntax for printing numeric values 
+print(360)
+print(89*56)
+
+#syntax for printing the value of a variable
+a=10
+print(a)
