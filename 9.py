a15220d916991b6df09008319c9b095adc142f85
AMAN-SONI-55566 <amanoffcial55566@gmail.com>
2025-06-20 09:44:05 +0530

    assignment of values to the variable

---
+++ 9.py
@@ -0,0 +1,9 @@
+# using variable with assignment and arithemetic operators 
+
+#assignment of values to the variable
+years=10
+weeks_in_years=52
+# This variable is assigned an arithmetic calculation:
+weeks_in_a_deacde=years*weeks_in_years
+# Prints the calculation stored in the "weeks_in_a_decade" variable:
+print(weeks_in_a_deacde)
