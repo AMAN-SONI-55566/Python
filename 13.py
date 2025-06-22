037bcdfc0317faae0090b12f371ee584599ff742
AMAN-SONI-55566 <amanoffcial55566@gmail.com>
2025-06-22 17:29:34 +0530

    area of triangle

---
+++ 13.py
@@ -0,0 +1,11 @@
+# calculating area of triangle
+
+def area_traingle(base ,height):
+    return base*height/2
+
+area_1=area_traingle(3,9)
+area_2=area_traingle(21,7)
+
+sum= area_1 + area_2
+print("the sum of both area is :" +str(sum))
+
