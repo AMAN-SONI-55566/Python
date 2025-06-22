a51666e7732367da573453ab388c772b356c9a11
AMAN-SONI-55566 <amanoffcial55566@gmail.com>
2025-06-22 07:01:38 +0530

    built-in-fn text file added

---
+++ 12.py
@@ -0,0 +1,19 @@
+# This follow-along reading is organized to match the content in the video that follows. 
+# It contains the same code shown in the next video. These code blocks will provide you 
+# with the opportunity to see how the code is written, allow you to practice running it, 
+# and can be used as a reference to refer back to. 
+
+# example1
+def greeting(name):
+    print("Welcome, " + name)
+    
+greeting("Kay")
+greeting("Cameron")
+
+# example2
+def greeting(name, department):
+    print("Welcome, " + name)
+    print("You are part of " + department)
+    
+greeting("Blake", "Software engineering")
+greeting("Ellis", "Software engineering")
