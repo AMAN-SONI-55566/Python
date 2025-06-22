9cdfa4d612e38f04e1b471b788fce17045ffd681
AMAN-SONI-55566 <amanoffcial55566@gmail.com>
2025-06-22 17:49:27 +0530

    none data type

---
+++ must read notes/15.py
@@ -0,0 +1,6 @@
+# none: special data type in python used to indicate that things are empty or that they returned nothing
+def greeting(name):
+    print("welcome," + name ) 
+
+result=greeting("Aman Soni")
+print(result)
